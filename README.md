07b15773ea9baed6cf16ed7413c7b68456f9c1db
ShingoFukuyama <houseshou@gmail.com>
2014-01-22 16:23:33 +0900

    Fix file path name for various OSs

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ Keep directories you've opened in dired, even if you stop running Emacs!
 
 ;; You can change the default file name and location. I recommend to attach
 ;; `user-full-name' to distinguish multiple systems that you are using.
-(setq helm-recentd-file (concat "~/.emacs.d/.helm-recentd" "-" user-full-name))
+(setq helm-recentd-file
+      (convert-standard-filename
+       (concat "~/.emacs.d/.helm-recentd" "-" user-full-name)))
 
 ;; Sort directory list by 'date or 'frequency
 (setq helm-recentd-sort 'date)
--- helm-recentd.el
+++ helm-recentd.el
@@ -29,7 +29,9 @@
 
 ;; ;; You can change the default file name and location. I recommend to attach
 ;; ;; `user-full-name' to distinguish multiple systems that you are using.
-;; (setq helm-recentd-file (concat "~/.emacs.d/.helm-recentd" "-" user-full-name))
+;; (setq helm-recentd-file
+;;       (convert-standard-filename
+;;        (concat "~/.emacs.d/.helm-recentd" "-" user-full-name)))
 
 ;; ;; Sort directory list by 'date or 'frequency
 ;; (setq helm-recentd-sort 'date)
@@ -49,7 +51,8 @@
 (defvar helm-recentd-list nil)
 (defvar helm-recentd-coding-system 'utf-8)
 
-(defcustom helm-recentd-file (concat "~/.helm-recentd" "-" user-full-name)
+(defcustom helm-recentd-file
+  (convert-standard-filename (concat "~/.helm-recentd" "-" user-full-name))
   "File path for helm-recentd"
   :group 'helm-recentd
   :type 'string)
